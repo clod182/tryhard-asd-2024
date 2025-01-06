ea1073b6b303d9aca3fba37c95928f4e92a79140
clod182_zorin_pc <clod182@hotmail.com>
2025-01-06 13:32:54 +0100

    -done es 4 and init es 5 of first esercitazione of last year

--- ASD_2024/esercitazioni_anni_passati/2023-24_esercitazione1_es5.cpp
+++ ASD_2024/esercitazioni_anni_passati/2023-24_esercitazione1_es5.cpp
@@ -5,17 +5,31 @@ using namespace std;
 
 /* TESTO
 Esercizio 5
-Dato un albero binario, scrivere una funzione che costruisce un array bidimensionale mat tale che, 
-per ogni coppia di nodi u e v, l’elemento mat[u][v] sia il minimo antenato comune di u e v. 
-La funzione deve richiedere tempo O(n^2) e il suo prototipo è:
+Nel centro di Preparazione Olimpica Acqua Acetosa "Giulio Onesti" si allenano la nazionale maschile e femminile di sollevamento pesi. 
+Purtroppo, anche qui può accadere che i pesi vengano rimessi nei posti sbagliati.
+Date n coppie di manubri con pesi distinti, i manubri sono posizionati senza un ordine particolare su due rastrelliere. 
+Inizialmente ogni fila ha un numero uguale di manubri. Trattandosi di una palestra professionale ben finanziata, 
+c'è spazio infinito alle due estremità di ciascuna rastrelliera per sostenere eventuali pesi aggiuntivi.
+Per spostare un manubrio, si può farlo rotolare in uno spazio vicino libero sulla stessa rastrelliera quasi senza sforzo, oppure si può prenderlo, 
+sollevarlo e posizionarlo in un altro posto libero; questo richiede forza proporzionale al suo peso.
+Qual è il peso più pesante che si deve sollevare per poter mettere pesi identici uno accanto all'altro? 
+Si tenga presente che su ciascuna rastrelliera dopo la riorganizzazione potrebbe esserci sistemato un numero diverso di pesi; questo è consentito.
 
-void minAntCom(PNode r, vector<vector<int>>& mat)
-Per semplicità si assuma che i nodi abbiano chiavi numerate da 0 a n-1 e che queste chiavi identifichino il nodo. 
-Di conseguenza gli indici della matrice rappresentano i nodi e in posizione mat[u][v] c'è la chiave del nodo 
-che è il minimo antenato comune di u e v.
-
-[Il minimo antenato comune di due nodi u e v è  l’antenato comune  di u e v che si trova più lontano dalla radice dell’albero.]
+Il prototipo della funzione è:
+int PesoMinimo(vector<int> pesi_1, vector<int> pesi_2)
 Analizzare e motivare in modo chiaro, preciso ed approfondito la complessità della funzione.
+
+Esempio:
+
+input:                       output:
+5
+2 1 8 2 8                   2
+9 9 4 1 4
+
+input:                       output:
+8
+7 7 15 15 2 2 4 4      0
+5 5 3 3 9 9 1 1
 */
 
 /*#region utilities functions*/
